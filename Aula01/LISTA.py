71777fc11276ec853131d475ef4da49a2d196990
Seu Nome Correto <seu-email@exemplo.com>
2026-07-22 11:33:54 -0300

    Adicionando desafio_lista.py

--- Aula01/LISTA.py
+++ Aula01/LISTA.py
@@ -1,8 +1,28 @@
+#Criando uma lista de modelos de carros 
 carros = ["gol","corsa","parati","fit","asx","fox"]
-print(f"Carro: {carros[0]}")
+print(f"Carro na primeira vaga: {carros[0]}")
 
-
-
-#Trocar o  opala por Gol AP
+#Trocar o elemento do indice 0 "gol" por "Gol AP"
 carros[0]= "Gol AP"
-print(f"Carro: {carros[0]}")
+print(f"Carro na primeira vaga: {carros[0]}")
+
+#Munipulação de listas(aumentar ou diminuir ou zerar)
+#o \n no final do input  serve para quebrar uma linha 
+novo_carro= str(input("Qual carro você deseja por na garagem\n"))
+carros.append(novo_carro)
+print(f"seu novo carro é um: {novo_carro}")
+print(carros)
+
+remover_carro=input("Qual carro você quer tirar da garagem\n")
+#se o carro não existir vai gerar erros 
+#verificar se o carro esta na lista
+if (remover_carro in carros):
+    carros.remove(remover_carro)
+    print(f"você removeu o carro: {remover_carro}")
+    print(carros)
+
+#verificar se o carro esta na lista
+if ("parati" in carros):
+    print("parati está na garagem.")
+else:
+    print("parati não esta na garagem.")
---
+++ Aula01/desafio_lista.py
@@ -0,0 +1,30 @@
+#um Um deerminado time pode ter no maximo 5 jogadores titulares.
+#1. criar uma lista com 4 jogadores (str)
+#2. Perguntar para usuário o nome de im jogador e incluir na lista (input e appeed)
+#3. não permitir que o time passe de 5 jogadores 
+#3.1 para descobrir quantos jogadores ja então na lista, use a função len()
+#3.2 montar um if, verifique se o time ja possui 5 jogadores, caso verdade exibir a mensagem:
+# "time completo ", caso contrario, exibir a mesagem "time está incompleto" 
+#4. Subtituir o primeiro jogador da lista po um novo nome digitado pelo usuario 
+#5. Pesquisar se o jogador está no time, caso sim, mostrar o nome dele ex: "O jogador está no time"
+
+jogadores_titulares= ["Leandro","Bernardo", "Leonid","Deusdette"]
+novo_jogador=str(input("Digite o nome do jogador\n"))
+jogadores_titulares.append(novo_jogador)
+print(f"O novo jogador escalado é: {novo_jogador}")
+if (len(jogadores_titulares)>=5):
+    print(f"Time está completo")
+    print(f"Seleção formada por: {[jogadores_titulares]}")
+else:
+    print("Time está incompleto")
+
+altera_jogador= str(input("Digite o nome do novo jogador: "))
+jogadores_titulares[0]= altera_jogador
+print(f"o jogador do indice 0 foi substituido por {jogadores_titulares[0]}")
+print(f"Seleção atual formada por: {[jogadores_titulares]}")
+
+pesquisa= str(input("Pesquise um jogador\n"))
+if(pesquisa in jogadores_titulares):
+    print(f"O jogador {pesquisa} está na lista oficial!")
+else:
+    print(f"O jogador não está na lista")
