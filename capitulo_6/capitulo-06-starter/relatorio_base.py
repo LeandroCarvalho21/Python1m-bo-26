61e4d614f5223bf02b375be8b6770d878431b5d2
Adriano Moura <adr.moura21@hotmail.com>
2026-07-27 11:47:46 -0300

    criando um for com len e range para exibir uma  duas listas

--- capitulo_6/capitulo-06-starter/relatorio_base.py
+++ capitulo_6/capitulo-06-starter/relatorio_base.py
@@ -18,8 +18,31 @@ for produto in produtos:
     
 
 
-# quero exibbir números de 1 até 100
+# quero exibir números de 1 até 100 com range
 for numero in range (0,101):
-    # numero+=1
-    # print(f"Número: {numero}")
-    print(f"Número: {numero}")
+    print(f"Números: {numero}")
+
+
+# conjunto de movimentações de pix
+movimentos = [-300, 1500, -600, 400]
+entradas = 0
+saidas = 0
+ 
+# contar trasações do tipo SAIDA e ENTRADA.
+for movimento in movimentos:
+    if (movimento > 0):
+        print(f"Transação de ENTRADA: R$: {movimento}")
+        entradas += 1
+    else:
+        print(f"Transação de saida: R$: {movimento}")
+        saidas += 1
+print(f"Total de entradas: {entradas}")
+print(f"Total de saidas: {saidas}")
+
+
+# cursos do  senai
+cursos = ["excel", "python", "power Bi"]
+carga_horaria = [80,60,24] #Representa horas
+
+for i in range(len(cursos)):
+    print(f"O curso: {cursos[i]}, tem uma duração de {carga_horaria[i]} horas ")
