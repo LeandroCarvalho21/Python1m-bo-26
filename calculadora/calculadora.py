e92601926dbe50235b55f52e3b776c7f4c4bc3cf
Lucas Carvalho <lucas.290sabino@gmail.com>
2026-08-12 08:50:28 -0300

    Criando uma função para pedir o o número para o usuario

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,5 +1,10 @@
 def mostrar_menu():
     print("\n=======Opções=======\n 1 - Adição \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 0 - Sair \n")
+    
+def pedir_numeros():    
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    return num1, num2
 
 def somar(a,b):
     calculo = a+b
@@ -24,26 +29,22 @@ def main():
         mostrar_menu()
         resposta = input("Escolha uma opção: ")
         if (resposta =='1' ):
-            num1 = float(input("Digite o primeiro número: "))
-            num2 = float(input("Digite o segundo número: "))
+            num1,num2 = pedir_numeros()
             resultado_da_calculadora = somar(num1,num2)
             print(F"A Adilção de {num1} + {num2} = {resultado_da_calculadora}")
             
         elif(resposta == '2'):
-            num1 = float(input("Digite o primeiro número: "))
-            num2 = float(input("Digite o segundo número: "))
+            num1,num2 = pedir_numeros()
             resultado_da_calculadora = sub(num1,num2)
             print(F"A Subtração de {num1} - {num2} = {resultado_da_calculadora}")
             
         elif(resposta == '3'):
-            num1 = float(input("Digite o primeiro número: "))
-            num2 = float(input("Digite o segundo número: "))
+            num1,num2 = pedir_numeros()
             resultado_da_calculadora = mult(num1,num2)
             print(F"A Multiplicação de {num1} * {num2} = {resultado_da_calculadora}")
             
         elif(resposta == '4'):
-            num1 = float(input("Digite o primeiro número: "))
-            num2 = float(input("Digite o segundo número: "))
+            num1,num2 = pedir_numeros()
             resultado_da_calculadora = div(num1,num2)
             print(F"A Divisão de {num1} / {num2} = {resultado_da_calculadora}")
             
